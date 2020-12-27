c4255c61d9f81e4f57552e5b45e47f1ad8bea746
sontakketejas9441 <76654419+sontakketejas9441@users.noreply.github.com>
2020-12-27 23:15:50 +0530

    Update Application.java

--- src/main/java/com/ts/Application.java
+++ src/main/java/com/ts/Application.java
@@ -9,6 +9,8 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("Hello There");
+		System.out.println("Addition");
+		
 	}
 
 }
